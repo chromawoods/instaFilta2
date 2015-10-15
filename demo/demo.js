53ec1c951480f57798816361af26b74999c466e0
Andreas Larsson <andreas@chromawoods.com>
2015-10-15 22:10:19 +0200

    Added the first examples of the demo page.

---
+++ demo/demo.js
@@ -0,0 +1,31 @@
+$(function() {
+
+  $('#ex1').instaFilta();
+
+  $('#ex2').instaFilta();
+
+  $('#ex3').instaFilta({
+    targets: '.planet-name',
+    caseSensitive: true
+  });
+
+  $('#ex4').instaFilta({ markMatches: true });
+
+  $('#ex5').instaFilta({ beginsWith: true });
+
+  $('#ex6').instaFilta();
+
+  var $resultMessage = $('#some-result-message');
+
+  $('#ex7').instaFilta({
+    onFilterComplete: function(matchedItems) {
+
+      var message = matchedItems.length
+        ? "I found " + matchedItems.length + " matches!"
+        : "I couldn't find a thing..";
+
+      $resultMessage.text(message);
+    }
+  });
+
+});
